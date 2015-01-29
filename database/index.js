23bbaf02b52ba386f11fd9e1024676da8b9eb287
tonylucas <tony3lucas@hotmail.fr>
2015-01-29 12:03:25 +0100

    Check local or Heroku

--- database/index.js
+++ database/index.js
@@ -1,7 +1,8 @@
 'use strict'
+console.log(process.env);
 
 var mongoose = require('mongoose');
-var mongolabStringConnexion = 'mongodb://localhost:27017/ws-restapi';
+var mongolabStringConnexion = process.env.MONGO_STRING_CONNECTION  ||  'mongodb://localhost:27017/ws-restapi';
 
 mongoose.connect(mongolabStringConnexion);
 
